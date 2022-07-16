354ad8e7cabdf07a9d9d5ab8b7cb17fa8f694146
Владислав Смаилов <vladislav.smailov@yandex.ru>
2022-07-17 00:33:10 +0300

    api for Reviews and Comments created

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,25 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, GenreViewSet, TitleViewSet
+from .views import (
+    CategoryViewSet,
+    GenreViewSet,
+    TitleViewSet,
+    ReviewViewSet,
+    CommentViewSet
+)
 
 router = DefaultRouter()
 router.register(r'category', CategoryViewSet, basename='category')
 router.register(r'genre', GenreViewSet, basename='genre')
 router.register(r'title', TitleViewSet, basename='title')
-
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<reviews_id>\d+)/comments/',
+    CommentViewSet, basename='comments'
+)
 app_name = 'api'
 urlpatterns = [
     path('v1/', include(router.urls)),
